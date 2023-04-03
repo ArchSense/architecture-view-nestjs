b5af13063b839318e29c158487baef46b33e75c5
Sergey Bolshchikov <sergey@bolshchikov.net>
2023-04-03 20:13:02 +0300

    feat(extension): choose the project in monorepo

--- src/extension.ts
+++ src/extension.ts
@@ -7,16 +7,39 @@ export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand('archsense.showArchitecture', async () => {
     send({ action: BI_ACTIONS.start });
 
-    const folders = vscode.workspace.workspaceFolders;
-    if (!folders?.length) {
+    const roots = await vscode.workspace.findFiles('**/src/main.ts', '**/node_modules/**');
+
+    if (!roots || roots.length === 0) {
+      console.log('Archsense: the workspace is empty');
       return;
     }
 
-    const rootFolder = folders[0];
+    const rootFoldersMap = roots.reduce((acc, curr) => {
+      const parts = curr.path.split('/');
+      const rootFolderName = parts.at(-3) as string;
+      const rootFolderPath = parts.slice(0, -2).join('/');
+      acc[rootFolderName] = rootFolderPath;
+      return acc;
+    }, {});
+
+    let rootFolderPath = Object.values(rootFoldersMap)[0] as string;
+
+    if (Object.values(rootFoldersMap).length > 1) {
+      // show the picker only in mono-repo several projects
+      const rootAppName = await vscode.window.showQuickPick(Object.keys(rootFoldersMap), {
+        canPickMany: false,
+        placeHolder: 'Looks like you have several projects here, which one would you like to see?',
+      });
+
+      if (!rootAppName) {
+        return;
+      }
+      rootFolderPath = rootFoldersMap[rootAppName];
+    }
 
     try {
       send({ action: BI_ACTIONS.parserStart });
-      const analysis = await analyze(rootFolder.uri.path);
+      const analysis = await analyze(rootFolderPath);
       send({ action: BI_ACTIONS.parserSuccess, payload: analysis });
       const currentView = ArchitectureViewPanel.createOrShow(context);
       currentView.onInit(() => {
@@ -36,4 +59,4 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-export function deactivate() {}
+export function deactivate() { }
