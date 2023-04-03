25e6cab83d5c64f2188082f2ed468529a3ad67a8
Sergey Bolshchikov <sergey@bolshchikov.net>
2023-04-03 16:33:39 +0300

    ci(test): support different  envs

--- test/e2e/single-project.e2e.ts
+++ test/e2e/single-project.e2e.ts
@@ -1,9 +1,9 @@
-import { browser, expect } from '@wdio/globals';
+import { Workbench } from 'wdio-vscode-service';
 
 describe('Architecture view extension', () => {
-  it('should render architecture for single root nestjs project', async () => {
-    const workbench = await browser.getWorkbench();
-    expect(await workbench.getTitleBar().getTitle()).toBe(
+  it('should load the workspace', async () => {
+    const workbench: Workbench = await browser.getWorkbench();
+    expect(await workbench.getTitleBar().getTitle()).toContain(
       '[Extension Development Host] guinea-pig-nestjs',
     );
   });
