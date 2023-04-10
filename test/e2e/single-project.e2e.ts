5d0b456f75bd88ee21b33357e2991143d9d758da
Sergey Bolshchikov <sergey@bolshchikov.net>
2023-04-10 15:54:01 +0300

    style(*): format

--- test/e2e/single-project.e2e.ts
+++ test/e2e/single-project.e2e.ts
@@ -1,7 +1,6 @@
 import { Workbench } from 'wdio-vscode-service';
 
 describe('Architecture view extension', () => {
-
   it('should load the workspace', async () => {
     const workbench: Workbench = await browser.getWorkbench();
     expect(await workbench.getTitleBar().getTitle()).toContain(
@@ -17,7 +16,7 @@ describe('Architecture view extension', () => {
     await archWebview.open();
     const nodes = await $$('.react-flow__node-actual');
     expect(nodes.length).toBe(7);
-    const nodesText = await Promise.all(nodes.map(node => node.getText()));
+    const nodesText = await Promise.all(nodes.map((node) => node.getText()));
     expect(nodesText).toContain('AppController\ngetHello');
   });
 });
